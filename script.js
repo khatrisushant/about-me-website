93ff370cc9447d376fedbbbae14d98378836b249
Sushant Khatri <your-email@example.com>
2026-03-21 23:15:48 -0500

    Made website modern and added dark mode

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const button = document.getElementById("colorBtn");
+const themeBtn = document.getElementById("themeBtn");
 
-button.addEventListener("click", function () {
-  document.body.style.backgroundColor = "#dff6ff";
+themeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    themeBtn.textContent = "☀️ Light Mode";
+  } else {
+    themeBtn.textContent = "🌙 Dark Mode";
+  }
 });
